9e9cc035ae7d3e7b3e0bb5a7986b2971e25549af
Aleksander Afanasyev <afonia.marcii@gmail.com>
2024-12-07 22:51:56 +0100

    update: increment version to 0.0.1, add Vue Router, and implement HomeView component

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import './style.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router/index';
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(router);
 app.use(pinia)
+
 app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Welcome to the Travel Blog</h1>
+    <p>This is the home page of the travel blog.</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: 'Hello, world!'
+    };
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
